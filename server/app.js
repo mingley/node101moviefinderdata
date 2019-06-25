d6a976c776b93f51ba5c8a41a4a36daea622a33c
mikeingley <michael.ingley@gmail.com>
2019-06-25 13:15:29 -0700

    first

--- server/app.js
+++ server/app.js
@@ -1,8 +1,32 @@
+//build server
 const express = require('express');
-
-
+const morgan = require('morgan');
+const axios = require('axios');
+const cache = {};
+//define port
+const port = 3000;
+//invoke express
 const app = express();
+app.use(morgan('dev'));
 
-// When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
+app.get('/', function(req, res){
+    if(req.query.hasOwnProperty('i')){
+        var movieId = req.query.i;
+        var omdb = 'http://www.omdbapi.com/?i=';
+        var info = omdb + movieId + '&apikey=8730e0e';
+    } if(req.query.hasOwnProperty('t')){
+        var movieId = req.query.t;
+        var omdb = 'http://www.omdbapi.com/?t=';
+        var info = omdb + movieId + '&apikey=8730e0e';
+    } if(cache.hasOwnProperty(info)){
+        res.send(cache[info]);
+        res.status(200);
+    } else{
+        axios.get(info).then(function(response){
+            cache[info] = response.data;
+            res.send(cache[info]);
+
+        })};
+});
 
 module.exports = app;
